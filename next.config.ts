383cfc901c1f49aa8516113120ecc440249e99b1
moufatmi <mou.fatmi@edu.umi.ac.ma>
2025-07-01 20:32:05 +0100

    'update'

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/asset-ledger',
+  assetPrefix: '/asset-ledger/',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
